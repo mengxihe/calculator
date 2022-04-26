63ab3d8cf3418f51ab776beb060e0abf33281209
Mengxi He <mengxihex@gmail.com>
2022-04-26 20:46:18 +0200

    html set up and operator function

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ const add = (a, b) => {
 const subtract = (a, b) => {
     return a-b;
 }
+
+const multiply = (a, b) => {
+    return a*b;
+}
+
+const divide = (a, b) => {
+    return a/b;
+}
+
+// console.log(add(2, 8));
+// console.log(subtract(3, 1));
+// console.log(multiply(3, 3));
+// console.log(divide(3, 2));
+const operate = (operator, a, b) => {
+    if (operator === add){
+        return add(a, b);
+    }
+    else if (operator === subtract) {
+        return subtract(a, b);
+    }
+    else if (operator === multiply) {
+        return multiply(a, b);
+    }
+    else if (operator === divide) {
+        return divide(a, b);
+    }
+}
+
+console.log(operate(divide, 4, 3));
+
